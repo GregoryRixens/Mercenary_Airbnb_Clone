a2bc58f6b61d07b5dd83c0be7837f508b8af2903
Johan FILONNEAU <filonovjohan@gmail.com>
2023-11-20 15:46:45 +0100

    update

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -304,6 +304,9 @@ Devise.setup do |config|
   # Note: These might become the new default in future versions of Devise.
   config.responder.error_status = :unprocessable_entity
   config.responder.redirect_status = :see_other
+  config.mailer_sender = 'your_mail@example.com'
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
+
 
   # ==> Configuration for :registerable
 
