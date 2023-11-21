821ec00c924060efaef3f8fe77715c7af61fd9b4
Raphael Wais <raphael.wais@gmail.com>
2023-11-21 22:19:35 +0100

    add doventrails

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,6 +1,8 @@
 class Offer < ApplicationRecord
   belongs_to :user
   has_many :reservations
+  has_many_attached :photos
+
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
