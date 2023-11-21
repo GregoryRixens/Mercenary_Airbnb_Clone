8f4a394a4587d152d422c72f4c3b8e79f0180a1b
Raphael Wais <raphael.wais@gmail.com>
2023-11-21 12:42:38 +0100

    Controller offer + seed offer & user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,24 @@
-puts 'Creating 10 fake offers...'
+puts 'Creating 10 users and each user has an offer...'
 10.times do
+  user = User.new(
+    user_name: Faker::Games::WorldOfWarcraft.hero,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 8),
+    # to test = email: Faker::Internet.email(name: :user_name)
+    # poster_url: Faker::LoremFlickr.image(search_terms: ['cat']),
+    last_name: Faker::Games::ElderScrolls.last_name,
+    first_name: Faker::Books::TheKingkillerChronicle.character
+  )
+  user.save!
   offer = Offer.new(
     title: Faker::Fantasy::Tolkien.poem,
     description: Faker::Books::Lovecraft.paragraphs(number: 2),
     # poster_url: Faker::LoremFlickr.image(search_terms: ['cat']),
     price: Faker::Number.between(from: 50, to: 200),
     address: Faker::Books::Dune.planet,
+    user_id: user.id
   )
-end
-puts 'Finished!'
-
-puts 'Creating 10 users...'
-10.times do
-  user = User.new(
-    user_name: Faker::Games::WorldOfWarcraft.hero,
-    email: Faker::Internet.email,
-    # to test = email: Faker::Internet.email(name: :user_name)
-    # poster_url: Faker::LoremFlickr.image(search_terms: ['cat']),
-    last_name: Faker::Games::ElderScrolls.last_name,
-    first_name: Faker::Books::TheKingkillerChronicle.character,
-  )
+  offer.save!
 end
 puts 'Finished!'
 
